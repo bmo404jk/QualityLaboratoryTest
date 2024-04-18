e49a8e17bc8fde74833f039e3f89a7d6a5bb9794
bmo404jk <78249495+bmo404jk@users.noreply.github.com>
2024-04-19 03:58:58 +0400

    LoginPage added

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -7,5 +7,36 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 public class LoginPage extends BasePage
 {
-
+    private final String URL = "https://mail.ru/";
+    private final String LOGIN = "testivanovivan2024";
+    private final String PASSWORD = "F5gh78@e90";
+    @FindBy(xpath = "//button[@class='resplash-btn resplash-btn_primary resplash-btn_mailbox-big aqjlcbn__1jdtl7f']")
+    private WebElement firstEnterButton;
+    @FindBy(xpath = "//input[@name='username']")
+    private WebElement accountNameInput;
+    @FindBy(xpath = "//input[@name='password']")
+    private WebElement accountPasswordInput;
+    @FindBy(xpath = "//button[@data-test-id='next-button']")
+    private WebElement accountPasswordInputButton;
+    @FindBy(xpath = "//button[@data-test-id='submit-button']")
+    private WebElement secondEnterButton;
+    @FindBy(xpath = "//iframe[@class='ag-popup__frame__layout__iframe']")
+    private WebElement iframe;
+    public void LoginToAccount()
+    {
+        firstEnterButton.click();
+        driver.switchTo().frame(iframe);
+        accountNameInput.click();
+        accountNameInput.sendKeys(LOGIN);
+        accountPasswordInputButton.click();
+        accountPasswordInput.click();
+        accountPasswordInput.sendKeys(PASSWORD);
+        secondEnterButton.click();
+        driver.navigate().refresh();
+    }
+    public LoginPage()
+    {
+        PageFactory.initElements(driver,this);
+        driver.get(URL);
+    }
 }
