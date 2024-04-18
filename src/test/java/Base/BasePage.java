067cd722300c46259388e6ef57eb78f4f37324f4
bmo404jk <78249495+bmo404jk@users.noreply.github.com>
2024-04-19 03:54:12 +0400

    BasePage and BaseTest added

--- src/test/java/Base/BasePage.java
+++ src/test/java/Base/BasePage.java
@@ -2,5 +2,9 @@ package Base;
 import org.openqa.selenium.WebDriver;
 public class BasePage
 {
-
+    protected static WebDriver driver;
+    public static void setDriver(WebDriver webDriver)
+    {
+        driver = webDriver;
+    }
 }
--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -6,5 +6,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 public abstract class BaseTest
 {
-
+    protected static WebDriver driver = new ChromeDriver();
+    @BeforeAll
+    public static void driverConfiguration()
+    {
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        BasePage.setDriver(driver);
+    }
+    @AfterEach
+    public void driverClose()
+    {
+        driver.close();
+        driver.quit();
+    }
 }
