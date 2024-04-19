b69523b1d3677ec263152582a9ec90e3d50bb8d8
bmo404jk <78249495+bmo404jk@users.noreply.github.com>
2024-04-19 04:03:28 +0400

    Creating EnterAccountTest

--- src/test/java/Tests/MainTest.java
+++ src/test/java/Tests/MainTest.java
@@ -6,5 +6,10 @@ import org.junit.jupiter.api.Test;
 
 public class MainTest extends BaseTest
 {
-
+    @Test
+    public void enterAccountTest()
+    {
+        LoginPage loginPage = new LoginPage();
+        loginPage.LoginToAccount();
+    }
 }
